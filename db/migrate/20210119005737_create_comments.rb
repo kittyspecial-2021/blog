a431559d85aeeef93bbca3952383f9674c023953
kittyspecial-2021 <zhang_kun@msn.com>
2021-01-23 19:57:50 -0800

    Fix the foreign key test failure

--- db/migrate/20210119005737_create_comments.rb
+++ db/migrate/20210119005737_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.references :post, null: false, foreign_key: true
+      t.references :post, null: false, index:true, foreign_key: {on_delete: :cascade}
       t.text :body
 
       t.timestamps
